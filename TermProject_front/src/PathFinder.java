f1a679e793e27906224014e2c8a3d042d7ed17f7
Superhankyu <kim0112hg@naver.com>
2021-05-26 15:31:43 +0900

    File directory moved.

--- TermProject_front/src/PathFinder.java
+++ TermProject_front/src/PathFinder.java
@@ -2,34 +2,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class PathFinder {
-	private List<Vertex> vertexs;
-	private List<Building> buildings;
-	private List<Room> rooms;
-
-	public PathFinder(){
-		// vertex read
-		// add edges
-		// add buildings
-		// add rooms
-	}
-
-	// When finding user position
+	// 자신 위치 마우스로 찾을 때,
 	public Vertex findClosestVertex(int x, int y) {
 		return new Vertex("test", new Position(x, y));
 	}
-
-	// When finding user position
-	public Building findClosestBuilding(int x, int y) {
-		return null;
-	}
 	
-	// When finding target position on click, target to building
+	// target을 마우스로 찾을 때, 건물 기준으로
 	public List<Vertex> findShortestPath(Vertex source, Building target) {
 		List<Vertex> path = new ArrayList<>();
 		return path;
 	}
 	
-	// When finding target position with category
+	// target을 category로 찾을 때,
 	public List<Vertex> findShortestPath(Vertex source, String category) {
 		List<Vertex> path = new ArrayList<>();
 		return path;
@@ -39,8 +23,9 @@ public class PathFinder {
 		return new Room();
 	}
 	
-	// Set buildings info when initilizing
+	// 처음에 건물 정보를 전부 가져와서 set 해둔다.
 	public List<Building> getAllBuildingInfos(){
+		List<Building> buildings = new ArrayList<>();
 		return buildings;
 	}
 }
